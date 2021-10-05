ffdb204f239422d43be2937efc25251681c1bd00
Ross Cumming <rosso5792@gmail.com>
2021-10-05 19:14:43 +0100

    background.js conditionals refactor

--- background.js
+++ background.js
@@ -3,10 +3,10 @@ import {
   youtube,
   reddit,
   amazonUK,
-} from './helpers/contextMenu.js';
-import { searchYoutube } from './helpers/searchYoutube.js';
-import { searchReddit, searchOldReddit } from './helpers/searchReddit.js';
-import { searchAmazonUK, searchAmazonUS } from './helpers/searchAmazon.js';
+} from "./helpers/contextMenu.js";
+import { searchYoutube } from "./helpers/searchYoutube.js";
+import { searchReddit, searchOldReddit } from "./helpers/searchReddit.js";
+import { searchAmazonUK, searchAmazonUS } from "./helpers/searchAmazon.js";
 
 chrome.runtime.onInstalled.addListener(() => {
   try {
@@ -22,28 +22,20 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
   }
 
   if (reddit.id === info.menuItemId) {
-    chrome.storage.sync.get(['OPTIONS'], result => {
-      if (result.OPTIONS.oldReddit) {
-        searchOldReddit(info);
-      } else {
-        searchReddit(info);
-      }
+    chrome.storage.sync.get(["OPTIONS"], (result) => {
+      result.OPTIONS.oldReddit ? searchOldReddit(info) : searchReddit(info);
     });
   }
 
   if (amazonUK.id === info.menuItemId) {
-    chrome.storage.sync.get(['OPTIONS'], result => {
-      if (result.OPTIONS.amazonUS) {
-        searchAmazonUS(info);
-      } else {
-        searchAmazonUK(info);
-      }
+    chrome.storage.sync.get(["OPTIONS"], (result) => {
+      result.OPTIONS.amazonUS ? searchAmazonUS(info) : searchAmazonUK(info);
     });
   }
 });
 
 chrome.storage.onChanged.addListener((changes, area) => {
-  if (area === 'sync' && changes.OPTIONS?.newValue) {
+  if (area === "sync" && changes.OPTIONS?.newValue) {
     Boolean(changes.OPTIONS.newValue.oldReddit);
     Boolean(changes.OPTIONS.newValue.amazonUS);
   }
