497100cf270cd692ec29ee9376825034d5b0139c
Ross Cumming <rosso5792@gmail.com>
2021-10-22 16:02:40 +0100

    sync for context menu works, need to implement toggle to hide on selection

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 import { createContextMenus } from './helpers/contextMenu.js';
 import { contextMenuChecks } from './helpers/contextMenuChecks.js';
-import { onInstallDefaultOptions } from './helpers/settings.js';
+import {
+  onInstallDefaultOptions,
+  checkSelectedMenuOptions,
+} from './helpers/settings.js';
 
 chrome.runtime.onInstalled.addListener(() => {
   try {
@@ -13,11 +16,19 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.contextMenus.onClicked.addListener(info => {
   contextMenuChecks(info);
+  checkSelectedMenuOptions(info);
 });
 
 chrome.storage.onChanged.addListener((changes, area) => {
   if (area === 'sync' && changes.OPTIONS?.DEFAULT_SETTINGS?.newValue) {
     Boolean(changes.OPTIONS.DEFAULT_SETTINGS.newValue.oldReddit);
     Boolean(changes.OPTIONS.DEFAULT_SETTINGS.newValue.amazonUS);
+  }
+
+  if (area === 'sync' && changes.OPTIONS?.CONTEXT_MENU?.newValue) {
+    Boolean(changes.OPTIONS.CONTEXT_MENU.newValue.enableReddit);
+    Boolean(changes.OPTIONS.CONTEXT_MENU.newValue.enableYoutube);
+    Boolean(changes.OPTIONS.CONTEXT_MENU.newValue.enableAmazon);
+    Boolean(changes.OPTIONS.CONTEXT_MENU.newValue.enableStackOverflow);
   }
 });
